be3af16624c6c06c1914d87d7b23dd2efd8fd012
Patrick Hannon <patrick_hannon@hotmail.com>
2019-03-03 13:27:31 -0600

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Hannon.FreightEngine.Models;
+using System.Configuration;
+using System.Diagnostics;
+namespace Hannon.FreightEngine
+{
+    class Program
+    {
+        private static string _uSPSUserName;
+        private static string _uSPSPassword;
+        private static IFreightEngine _uspsFreightEngine;
+
+        static void Main(string[] args)
+        {
+            _uSPSUserName = ConfigurationManager.AppSettings["USPSUserName"];
+            _uSPSPassword = ConfigurationManager.AppSettings["USPSPassword"];
+
+            _uspsFreightEngine = new USPSManager(_uSPSUserName, true);
+            Address addr = new Address()
+            {
+                Address1 = "2438 Homewood Trail",
+                Address2 = string.Empty,
+                City = "Arlington",
+                State = "TX",
+                Zip = "76015",
+                ZipPlus4 = string.Empty,
+                Contact = "Patrick Hannon",
+                ContactEmail = string.Empty,
+                FirmName = String.Empty
+            };
+
+            var address = _uspsFreightEngine.ValidateAddress(addr);
+            Debug.WriteLine(address);
+        }
+    }
+}
---
+++ USPSManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Hannon.FreightEngine.Models;
+
+namespace Hannon.FreightEngine
+{
+    public class USPSManager : IFreightEngine
+    {
+        #region Private Members
+        private const string ProductionUrl = "http://production.shippingapis.com/ShippingAPI.dll";
+        private const string TestingUrl = "http://testing.shippingapis.com/ShippingAPITest.dll";
+        private WebClient web;
+        private string _userid;
+        #endregion
+
+        /// <summary>
+        /// Creates a new USPS Manager instance
+        /// </summary>
+        /// <param name="USPSWebtoolUserID">The UserID required by the USPS Web Tools</param>
+        public USPSManager(string USPSWebtoolUserID)
+        {
+            web = new WebClient();
+            _userid = USPSWebtoolUserID;
+            _TestMode = false;
+
+        }
+        /// <summary>
+        /// Creates a new USPS Manager instance
+        /// </summary>
+        /// <param name="USPSWebtoolUserID">The UserID required by the USPS Web Tools</param>
+        /// <param name="testmode">If True, then the USPS Test URL will be used.</param>
+        public USPSManager(string USPSWebtoolUserID, bool testmode)
+        {
+            _TestMode = testmode;
+            web = new WebClient();
+            _userid = USPSWebtoolUserID;
+        }
+
+        #region Properties
+        private bool _TestMode;
+        public bool TestMode
+        {
+            get { return _TestMode; }
+            set { _TestMode = value; }
+        }
+        #endregion
+
+        public Address ValidateAddress(Address address)
+        {
+            try
+            {
+                string validateUrl = "?API=Verify&XML=<AddressValidateRequest USERID=\"{0}\"><Address ID=\"{1}\"><Address1>{2}</Address1><Address2>{3}</Address2><City>{4}</City><State>{5}</State><Zip5>{6}</Zip5><Zip4>{7}</Zip4></Address></AddressValidateRequest>";
+                string url = GetURL() + validateUrl;
+                url = String.Format(url, _userid, address.ID.ToString(), address.Address1, address.Address2, address.City, address.State, address.Zip, address.ZipPlus4);
+                string addressxml = web.DownloadString(url);
+                if (addressxml.Contains("<Error>"))
+                {
+                    int idx1 = addressxml.IndexOf("<Description>") + 13;
+                    int idx2 = addressxml.IndexOf("</Description>");
+                    int l = addressxml.Length;
+                    string errDesc = addressxml.Substring(idx1, idx2 - idx1);
+                    throw new USPSManagerException(errDesc);
+                }
+
+                return Address.FromXml(addressxml);
+            }
+            catch (WebException ex)
+            {
+                throw new USPSManagerException(ex);
+            }
+        }
+        #region Private methods
+        private string GetURL()
+        {
+            string url = ProductionUrl;
+            if (TestMode)
+                url = TestingUrl;
+            return url;
+        }
+        #endregion
+    }
+}
